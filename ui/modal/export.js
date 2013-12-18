116efbc862b1b4e744bcbe9e40842f85eb571bc2
Nate Irwin <nate@nateirwin.net>
2013-12-18 15:00:55 -0700

    Link to new maps section

--- ui/modal/export.js
+++ ui/modal/export.js
@@ -47,7 +47,7 @@ Builder.ui.modal.export = (function() {
     if (mapId) {
       $('#cms-id').val(mapId);
       $('#modal-export-template img').on('click', function() {
-        window.open('maps/' + this.id.replace('template-', '') + '.html?id=' + mapId, '_blank');
+        window.open('/maps/' + this.id.replace('template-', '') + '.html?mapId=' + mapId, '_blank');
       });
       $('#modal-export ul a:first').tab('show');
       $(children[0]).removeClass('disabled');
